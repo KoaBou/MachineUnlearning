aed832809e6a0563a396178b4442e265e1f6d39a
KoaBou <nguyentrungnguyen20202004@gmail.com>
2024-05-30 23:43:39 +0700

    modify accuracy function

--- utils/utils.py
+++ utils/utils.py
@@ -3,10 +3,14 @@ from torch.nn import Module
 import torch
 
 
+
 def accuracy(net: Module, loader: DataLoader):
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
     correct = 0
     total = 0
+
+    net.to(DEVICE).eval()
 
     for inputs, labels in loader:
         inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
